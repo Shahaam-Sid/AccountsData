32807c32b88db814a3f26eb5e104ff6827cad3d3
Shahaam-Sid <muhammadgc821@gmail.com>
2025-08-03 22:56:57 +0500

    Added Data input and Saving to class file

---
+++ class.py
@@ -0,0 +1,56 @@
+import csv
+
+class User:
+    
+    filePath = "AccountsData/data.csv"
+    
+    def __init__(self, username):
+        self.username = username
+        self.email = ""
+        self.password = ""
+        
+    def getEmail(self):
+        self.email = input(f"{self.username} Enter your E-mail: ")
+    def getPassword(self):
+        passMatch = False
+        
+        while passMatch == False:
+            passOnce = input(f"Enter Password: ")
+            passTwice = input(f"Enter Password Again: ")
+            if passOnce == passTwice:
+                self.password = passOnce
+                passMatch = True
+            else:
+                print("Password not match, Try again")
+            
+    def __str__(self):
+        return f"Username: {self.username}, E-mail: {self.email}, Password: {self.password}"
+    
+    def loadData(self):
+        data = [self.username, self.email, self.password]
+        try:
+            with open(User.filePath, "a", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow(data)
+                print("Data Saved")
+        except FileNotFoundError:
+            print("File Does not exist")
+        except Exception:
+            print("An Error Occured")   
+            
+            
+            
+
+
+#testing
+
+username = input("Enter Your Username: ")
+
+x = User(username)
+
+x.getEmail()
+x.getPassword()
+
+print(x)
+
+x.loadData()
