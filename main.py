542e3102e5ec558e82d2d73f4ed384ae188973e0
Shahaam-Sid <muhammadgc821@gmail.com>
2025-08-29 02:55:41 +0500

    debugged 1st line repitation in csv file

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
-
 import csv
 from userdata import User
 
 data = ["username", "email", "password"]
 try:
-    with open(User.filePath, "a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow(data)
+    username = input("Enter Your Username: ").strip()
+
+    x = User(username)
+    x.getEmail()
+    x.getPassword()
+    x.loadData()
+    
+    
 except FileNotFoundError:
     print("File Does not exist")
 except Exception:
-    print("An Error Occured")               
-
-username = input("Enter Your Username: ").strip()
-x = User(username)
-
-x.getEmail()
-x.getPassword()
-x.loadData()
+    print("An Error Occured")
